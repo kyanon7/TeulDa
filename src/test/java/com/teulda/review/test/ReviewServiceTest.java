7268e14faf2dd1a663bff2cb902790d824d3bbee
KimCG1130 <corud1130@gmail.com>
2021-01-15 16:57:00 +0900

    corud/16:56/제발

--- src/test/java/com/teulda/review/test/ReviewServiceTest.java
+++ src/test/java/com/teulda/review/test/ReviewServiceTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.teulda.service.domain.Review;
 import com.teulda.service.review.ReviewService;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration	(locations = {	"classpath:config/context-common.xml",
 										"classpath:config/context-aspect.xml",
 										"classpath:config/context-mybatis.xml",
