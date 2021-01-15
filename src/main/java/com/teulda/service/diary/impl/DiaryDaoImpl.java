9cd8861d1b64a6e2e6410538903dfedb4a9240ff
Joowon Jung <jjoowon.jung@gmail.com>
2021-01-15 09:43:08 +0900

    zoo1 / 09:42 / Override

--- src/main/java/com/teulda/service/diary/impl/DiaryDaoImpl.java
+++ src/main/java/com/teulda/service/diary/impl/DiaryDaoImpl.java
@@ -1,7 +1,72 @@
 package com.teulda.service.diary.impl;
 
+import java.util.List;
+
+import com.teulda.common.Group;
+import com.teulda.common.Search;
 import com.teulda.service.diary.DiaryDao;
+import com.teulda.service.domain.Diary;
 
 public class DiaryDaoImpl implements DiaryDao {
+
+	@Override
+	public void addDiary(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addDiaryGroup(Group group) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Diary getDiary(int diaryNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Diary> getDiaryList(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getDiaryCount(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateDiary(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateDiaryStatus(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateDiaryGroup(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateDiaryViewCount(int diaryNo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteDiary(int diaryNo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- src/main/java/com/teulda/service/diary/impl/DiaryServiceImpl.java
+++ src/main/java/com/teulda/service/diary/impl/DiaryServiceImpl.java
@@ -1,7 +1,66 @@
 package com.teulda.service.diary.impl;
 
+import java.util.Map;
+
+import com.teulda.common.Group;
+import com.teulda.common.Search;
 import com.teulda.service.diary.DiaryService;
+import com.teulda.service.domain.Diary;
 
 public class DiaryServiceImpl implements DiaryService {
+
+	@Override
+	public void addDiary(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addDiaryGroup(Group group) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Diary getDiary(int diaryNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getDiaryList(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateDiary(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateDiaryStatus(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateDiaryGroup(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteDiary(int diaryNo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printDiary(Diary diary) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 }
