8271cad92d55488812738ec6c245db8cc3473ca0
kyanon7 <53827693+kyanon7@users.noreply.github.com>
2021-01-15 13:58:11 +0900

    cooldown / 13:57 / mapper 돌아간다 신난다

--- src/main/java/com/teulda/service/community/impl/CommunityServiceImpl.java
+++ src/main/java/com/teulda/service/community/impl/CommunityServiceImpl.java
@@ -13,48 +13,49 @@ import com.teulda.service.community.CommunityDao;
 import com.teulda.service.community.CommunityService;
 import com.teulda.service.domain.Bookmark;
 
-	@Service("communityServiceImpl")
+//	@Service("communityServiceImpl")
 	public class CommunityServiceImpl implements CommunityService {
 
-		//setter 인젝션	
-		@Autowired
-		@Qualifier("communityDaoImpl")
-		private CommunityDao communityDao;
-		
-		public void setCommunityDao(CommunityDao communityDao){
-			this.communityDao = communityDao;
-		}
-		
-		//디버깅을 위한
-		public CommunityServiceImpl() {  
-			System.out.println(this.getClass());
-		}
+//		//setter 인젝션	
+//		@Autowired
+//		@Qualifier("communityDaoImpl")
+//		private CommunityDao communityDao;
+//		
+//		public void setCommunityDao(CommunityDao communityDao){
+//			this.communityDao = communityDao;
+//		}
+//		
+//		//디버깅을 위한
+//		public CommunityServiceImpl() {  
+//			System.out.println(this.getClass());
+//		}
 	
 	
 	@Override
 	public void addBookmark(Bookmark bookmark) throws Exception {
 		
-		communityDao.addBookmark(bookmark);
+//		communityDao.addBookmark(bookmark);
 		
 	}
 
 	@Override
 	public void deleteBookmark(int bookmark) throws Exception {
 		
-		communityDao.deleteBookmark(bookmark);
+//		communityDao.deleteBookmark(bookmark);
 		
 	}
 
 	@Override
 	public Map<String, Object> getBookmarkList(Search search) throws Exception {
-		List<Bookmark> list = communityDao.getBookmarkList(search);
-		int totalCount = communityDao.getTotalCount(search);
-		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("list", list);
-		map.put("totalCount", totalCount);
-		
-		return map;
+//		List<Bookmark> list = communityDao.getBookmarkList(search);
+//		int totalCount = communityDao.getTotalCount(search);
+//		
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		map.put("list", list);
+//		map.put("totalCount", totalCount);
+//		
+//		return map;
+		return null;
 	}
 
 }
