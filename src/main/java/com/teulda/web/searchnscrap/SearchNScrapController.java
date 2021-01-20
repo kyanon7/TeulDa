135639330c4219552d0b2bf13c550287b576cb38
tjcpji <tjcpji1@naver.com>
2021-01-20 10:01:33 +0900

    tjcpji / 1001 / SearchNScrap(Rest)Controller 추가

---
+++ src/main/java/com/teulda/web/searchnscrap/SearchNScrapController.java
@@ -0,0 +1,9 @@
+package com.teulda.web.searchnscrap;
+
+public class SearchNScrapController {
+
+	public SearchNScrapController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/teulda/web/searchnscrap/SearchNScrapRestController.java
@@ -0,0 +1,9 @@
+package com.teulda.web.searchnscrap;
+
+public class SearchNScrapRestController {
+
+	public SearchNScrapRestController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
